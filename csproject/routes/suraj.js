f7adb984bfd9ecd66ea319719fa4f3e4b6d0ba98
Shubham Gupta <sguptadrums@gmail.com>
2019-09-25 12:32:49 -0700

    Removing Duplicate files
    Cleaning up the repo.

--- csproject/routes/suraj.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/suraj',function(req, res, next) {
-  //some mysql
-  res.render('suraj', { title: 'I am suraj' });
-});
-
-module.exports = router;
