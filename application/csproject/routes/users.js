aae0778e31f08eb9fc3de922183f821b59fdb3a4
Greg <gregsinbox@protonmail.com>
2019-10-03 17:50:59 -0700

    Made basic database logic for mysql npm library.

--- application/csproject/routes/users.js
+++ application/csproject/routes/users.js
@@ -13,7 +13,6 @@ connection.connect();
 //This will take any user related POST or GET requests, and save it into a 
 //user variable.
 //user = GET etc etc.
-//var mysql = 'SELECT SOMETHING SOMETHING ETC.' + user;
 //Here, we will connect to the database and pull data, which will
 //be encoded into a JSON object and placed into variable results
 //Ideally, all of this would be inside of a router call. 
@@ -21,16 +20,24 @@ connection.connect();
 
 /* GET users listing. */
 //this is totally separtae from main routes and will be used later when we add users -Greg
-router.get('/', function(req, res, next) {
+router.get('/login', function(req, res, next) {
+    var user = req.body.username;
     let error = "";
     //remember that this console.log goes to the server
-    console.log("test"); 
-    connection.query(mysql,function(error, results, fields){
+    let mysqlquery = 'SELECT * FROM `user` WHERE username = "Mike"';
+    let html = "";
+    connection.query(mysqlquery,function(error, results, fields){
      if(error) throw error;
-     error = 'test' + results[0].test;  
-     console.log('test' + results[0].test);
+     console.log(results);
+     if(results.length < 1){
+       html = "Please Sign Up";
+     }
+     else{
+       html =  "";
+     }
+     res.send(html);
+     //console.log('test' + results[0].test);
     });
-  res.send("I see that");
 });
 
 module.exports = router;
