11eb1839dd0db7e7bf0ecbbd319bdabcc60e49e3
Greg <gregsinbox@protonmail.com>
2019-10-03 23:55:59 -0700

    used mysql2 instead because it has built in JS promises for async.

--- application/csproject/routes/users.js
+++ application/csproject/routes/users.js
@@ -1,16 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-var mysql = require('mysql');
+/*var mysql = require('mysql');
 var connection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
     password : 'password',
     database : 'users'
 });
+*/
 
-connection.connect();
-//This will take any user related POST or GET requests, and save it into a 
+/*
+  connection.query('SELECT * FROM `user` WHERE `username` = ?',[user] ,function(error, results, fields){
+      if(error) throw error;
+      html = "stuff";
+      console.log(html);
+      if(results.length < 1) {
+          html = "Please Sign Up";
+      }
+      else{
+          html =  "Logged In!";
+      }
+  });
+ */
+//connection.connect();
+//This will take any user related POST or GET requests, and save it into a
 //user variable.
 //user = GET etc etc.
 //Here, we will connect to the database and pull data, which will
@@ -18,26 +32,26 @@ connection.connect();
 //Ideally, all of this would be inside of a router call. 
 //connection.end();
 
+async function dblogin(username,password) {
+    const mysql = require('mysql2/promise');
+    const connection = await mysql.createConnection({ host: 'localhost', user: 'root', password: 'password', database: 'users'});
+    const [rows,fields] = await connection.execute('SELECT * FROM `user` WHERE `username` = ?',[username]);
+    return [rows,fields];
+}
+
+async function dbregister(username,password) {
+    const mysql = require('mysql2/promise');
+    const connection = await mysql.createConnection({ host: 'localhost', user: 'root', password: 'password', database: 'users'});
+    //const [rows,fields] = await connection.execute('SELECT * FROM `user` WHERE `username` = ?',[username]);
+    return [rows,fields];
+}
 /* GET users listing. */
 //this is totally separtae from main routes and will be used later when we add users -Greg
 router.post('/loggedin' , function(req, res, next){
-  var user = req.body.username;
-  console.log(req);
-  let error = "";
-  //remember that this console.log goes to the server
-  let mysqlquery = 'SELECT * FROM `user` WHERE username = "Mike"';
-  let html = "";
-  connection.query(mysqlquery,function(error, results, fields){
-      if(error) throw error;
-      console.log(results);
-      if(results.length < 1) {
-          html = "Please Sign Up";
-      }
-      else{
-          html =  "";
-      }
-  });
-  res.send("You're in login page");
+  var user = req.body.loginusername;
+  let dbresults = dblogin(user,"Stuff");
+  console.log(dbresults);
+  res.send(dbresults);
 });
 
 router.post('/register' , function(req, res, next){
