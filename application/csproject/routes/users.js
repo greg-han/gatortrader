e21b47681a3d36641fd5a98d2f4368e89e935f32
Greg <gregsinbox@protonmail.com>
2019-10-04 00:47:22 -0700

    Fixed async issues, database working correctly. Simply need to add login and register logic. Don't forget to use something like bcyrpt for the password.

--- application/csproject/routes/users.js
+++ application/csproject/routes/users.js
@@ -1,42 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/*var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'password',
-    database : 'users'
-});
-*/
-
-/*
-  connection.query('SELECT * FROM `user` WHERE `username` = ?',[user] ,function(error, results, fields){
-      if(error) throw error;
-      html = "stuff";
-      console.log(html);
-      if(results.length < 1) {
-          html = "Please Sign Up";
-      }
-      else{
-          html =  "Logged In!";
-      }
-  });
- */
-//connection.connect();
-//This will take any user related POST or GET requests, and save it into a
-//user variable.
-//user = GET etc etc.
-//Here, we will connect to the database and pull data, which will
-//be encoded into a JSON object and placed into variable results
-//Ideally, all of this would be inside of a router call. 
-//connection.end();
 
 async function dblogin(username,password) {
     const mysql = require('mysql2/promise');
     const connection = await mysql.createConnection({ host: 'localhost', user: 'root', password: 'password', database: 'users'});
-    const [rows,fields] = await connection.execute('SELECT * FROM `user` WHERE `username` = ?',[username]);
-    return [rows,fields];
+    const rows = await connection.execute('SELECT * FROM `user` WHERE `username` = ?',[username]);
+    return rows;
 }
 
 async function dbregister(username,password) {
@@ -45,13 +15,29 @@ async function dbregister(username,password) {
     //const [rows,fields] = await connection.execute('SELECT * FROM `user` WHERE `username` = ?',[username]);
     return [rows,fields];
 }
+
 /* GET users listing. */
 //this is totally separtae from main routes and will be used later when we add users -Greg
-router.post('/loggedin' , function(req, res, next){
+router.post('/loggedin' , async function(req, res, next){
   var user = req.body.loginusername;
+  let resultbody = "";
+  let html = "";
   let dbresults = dblogin(user,"Stuff");
-  console.log(dbresults);
-  res.send(dbresults);
+  await dbresults.then(function(result){
+      //console.log("result: ",result[0]);
+      resultbody = result[0];
+      return result[0];
+  })
+  .catch(function(error){
+      console.error(error);
+  });
+  if(resultbody.length < 1){
+     html =  "user does not exist";
+  }
+  else{
+     html = "You are logged in!";
+  }
+  res.send(html);
 });
 
 router.post('/register' , function(req, res, next){
